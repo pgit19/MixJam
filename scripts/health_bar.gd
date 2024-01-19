505e0738d7b3f17dfcd2e8e4fb65f8dda76c9e6e
pgrummel <philipp.grummel@triology.de>
2024-01-19 22:44:56 +0100

    added mock health bar from other project

---
+++ scripts/health_bar.gd
@@ -0,0 +1,31 @@
+class_name HealthBar extends Control
+
+@export var ui_theme : Theme
+
+@onready var health_progress_bar = $HealthProgressBar
+@onready var delayed_progess_bar = $DelayedProgressBar
+
+const delayed_progress_bar_speed_per_second : float = 2.5
+
+
+func _ready():
+	if ui_theme:
+		theme = ui_theme
+
+
+func _process(delta):
+	var weight = min(delayed_progress_bar_speed_per_second * delta, 1.0)
+	var current_value = delayed_progess_bar.get_value()
+	var target_value = health_progress_bar.get_value()
+	var new_value = lerpf(current_value, target_value, weight)
+	delayed_progess_bar.set_value_no_signal(new_value)
+
+
+func set_max(p_value : float):
+	health_progress_bar.set_max(p_value)
+	delayed_progess_bar.set_max(p_value)
+	
+
+func set_value(p_value : float):
+	health_progress_bar.set_value_no_signal(p_value)
+	
