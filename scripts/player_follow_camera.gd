b1e122af04722326a4151d22c961b5545c63c0ef
pgrummel <philipp.grummel@triology.de>
2024-01-21 14:16:35 +0100

    made camera react to turns

--- scripts/player_follow_camera.gd
+++ scripts/player_follow_camera.gd
@@ -2,7 +2,8 @@ extends Camera2D
 
 @onready var player_instance = get_parent().get_node("Player")
 
-const camera_speed = 0.2
+const camera_speed = 0.1
+var is_player_turn : bool
 
 
 func _ready():
@@ -11,19 +12,24 @@ func _ready():
 
 
 func _process(delta):
-	position = position.lerp(player_instance.position, camera_speed)
+	if is_player_turn:
+		look_at_player()
+	else:
+		look_at_map()
+
+func look_at_player():
+	position = position.lerp(player_instance.position + Vector2(300, 0), camera_speed)
+	zoom = zoom.lerp(Vector2(0.8, 0.8), camera_speed)
 
 
-func determine_position():
-	#TODO: Make camera follow mouse a bit
-	var player_position = player_instance.position
-	var mouse_position = get_global_mouse_position()
-	return (player_position + mouse_position) / 2
-	
-	
+func look_at_map():
+	position = position.lerp(Vector2(1000, 0), camera_speed)
+	zoom = zoom.lerp(Vector2(0.5, 0.5), camera_speed)
+
+
 func _on_player_turn_started():
-	pass
+	is_player_turn = true
 
 
 func _on_enemy_turn_started():
-	pass
+	is_player_turn = false
